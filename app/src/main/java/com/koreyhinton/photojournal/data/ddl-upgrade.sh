d0dbce15f8694f2d88f7f5d54e0093f1ec95fb64
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2026-08-30 07:18:54 -0400

    typo

--- app/src/main/java/com/koreyhinton/photojournal/data/ddl-upgrade.sh
+++ app/src/main/java/com/koreyhinton/photojournal/data/ddl-upgrade.sh
@@ -7,7 +7,7 @@ ui=${DB_INITIAL_VERSION}
 while [[ $ui -le ${DB_VERSION} ]]
 do
     echo "if(oldVersion<${ui}) {"
-        echo "val ${v}DDl = \"\"\""
+        echo "val ${v}Ddl = \"\"\""
             ./schema${ui}.sh ${v} # assigns sql to kotlin ${v}Ddl var
          echo '"""'
         ./db-exec.sh ${v} # uses ${v}Ddl kotlin var to execute transaction
