de3f47c3865fcb46b73695b40b51d9c043927805
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2026-09-19 08:04:40 -0400

    feat: list s3 file counts per bucket

--- app/src/main/java/com/koreyhinton/photojournal/web/JSIface.sh
+++ app/src/main/java/com/koreyhinton/photojournal/web/JSIface.sh
@@ -4,6 +4,8 @@ v=${1:-jsi_}
 priv=${RANDOM}_
 # maps
 export ${v}${priv}_list_S3ClientBuild=${v}${priv}_build_S3ClientBuild
+export ${v}${priv}_objects_S3ClientBuild=${v}${priv}_build_S3ClientBuild
+export ${v}${priv}objects_S3Bucket=b
 
 cat << EOF
 
@@ -17,10 +19,12 @@ import com.koreyhinton.photojournal.models.S3ClientBuild
 import com.koreyhinton.photojournal.MainActivity
 import android.widget.LinearLayout
 import android.widget.EditText
+import android.widget.TextView
 import android.text.InputType
 import android.app.AlertDialog
 import android.content.DialogInterface
 import android.webkit.WebView
+import java.io.File
 
 class JSIface(
         private val context: Context,
@@ -33,18 +37,48 @@ class JSIface(
     }
     @JavascriptInterface
     fun signIn() {
+
+        val cacheFile = File(context.cacheDir, ".pj-s3-cache")
+        val unicodeFieldSep = "\u001F"
+
+        var cachedAccessKey: String? = null
+        var cachedRegion: String? = null
+        var cachedUrl: String? = null
+        if (cacheFile.exists()) {
+            val values = cacheFile.readText().split(unicodeFieldSep)
+            var i = 0;
+            if (values.size > i)
+                cachedAccessKey = values[i++]
+            if (values.size > i)
+                cachedRegion = values[i++]
+            if (values.size > i)
+                cachedUrl = values[i]
+        }
+
         activity.runOnUiThread {
             val layout = LinearLayout(activity).apply {
                 orientation = LinearLayout.VERTICAL
                 setPadding(48, 24, 48, 8)
             }
 
+            // Access Key
+            var accessKeyLbl = TextView(activity).apply {
+            }
+            accessKeyLbl.setText("Access Key")
+            layout.addView(accessKeyLbl)
             var accessKeyEditText = EditText(activity).apply {
                 hint = "Access key"
                 inputType = InputType.TYPE_CLASS_TEXT
             }
+            if (cachedAccessKey != null)
+                accessKeyEditText.setText(cachedAccessKey)
             layout.addView(accessKeyEditText)
 
+            // Secret Key
+            var secretKeyLbl = TextView(activity).apply {
+            }
+            secretKeyLbl.setText("Secret Key")
+            layout.addView(secretKeyLbl)
             val secretEditText = EditText(activity).apply {
                 hint = "Secret key"
                 inputType = InputType.TYPE_CLASS_TEXT or
@@ -52,21 +86,40 @@ class JSIface(
             }
             layout.addView(secretEditText)
 
+            // Region
+            var regLbl = TextView(activity).apply {
+            }
+            regLbl.setText("Region")
+            layout.addView(regLbl)
             var regEditText = EditText(activity).apply {
                 hint = "Region"
                 inputType = InputType.TYPE_CLASS_TEXT
             }
+            if (cachedRegion != null)
+                regEditText.setText(cachedRegion)
             layout.addView(regEditText)
 
+            // Url
+            var urlLbl = TextView(activity).apply {
+            }
+            urlLbl.setText("Url")
+            layout.addView(urlLbl)
             var urlEditText = EditText(activity).apply {
                 hint = "Url"
                 inputType = InputType.TYPE_CLASS_TEXT
             }
+            if (cachedUrl != null)
+                urlEditText.setText(cachedUrl)
             layout.addView(urlEditText)
 
             val conn = { dialog: DialogInterface, which: Int ->
 
                 Thread {
+                    cacheFile.writeText(
+                        accessKeyEditText.text.toString() + unicodeFieldSep +
+                        regEditText.text.toString() + unicodeFieldSep +
+                        urlEditText.text.toString()
+                    )
                     val ${v}${priv}build_S3ClientBuild = S3ClientBuild(
                         awsRegion = regEditText.text.toString(),
                         awsUrl = urlEditText.text.toString(),
@@ -77,13 +130,21 @@ class JSIface(
                     accessKeyEditText.text = null
                     urlEditText.text = null
                     regEditText.text = null
+
+                    var counts = mutableListOf<Int>()
+
                     ` ${ORC_S3}/snippets/build-client.sh ${v}${priv}build_ `
                     ` ${ORC_S3}/snippets/list-buckets.sh ${v}${priv}list_ `
+                    for (b in ${v}${priv}list_S3BucketCsv.split(",")) {
+                        ` ${ORC_S3}/snippets/list-files.sh ${v}${priv}objects_ `
+                        counts.add(
+                            ${v}${priv}objects_S3FilesCsv.split(",").count())
+                    }
 
                     activity.runOnUiThread {
                         var webV = activity.findViewById<WebView>(R.id.webby)
                         webV.post {
-                            webV.evaluateJavascript("document.write('"+${v}${priv}list_S3BucketCsv+"');", null)
+                            webV.evaluateJavascript("document.write('"+${v}${priv}list_S3BucketCsv+counts.joinToString(separator="-")+"');", null)
                         }
                     }
                 }.start()
